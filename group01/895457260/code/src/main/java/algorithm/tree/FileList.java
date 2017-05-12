01a76e9ad6779199964919d979c82f47e4280b27
Haochen <haochen_233333@163.com>
2017-05-12 20:04:03 +0800

    finish tree algorithm

--- group01/895457260/code/src/main/java/algorithm/tree/FileList.java
+++ group01/895457260/code/src/main/java/algorithm/tree/FileList.java
@@ -1,10 +1,32 @@
 package algorithm.tree;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class FileList {
-	public void list(File f) {		
-	}
 
-	
+    public void list(File f) {
+        list(f, 0);
+    }
+
+    private void list(File f, int depth) {
+        if (f == null) {
+            return;
+        }
+        System.out.println(inset(depth) + f.getName() + (f.isDirectory() ? " <dir>" : ""));
+        if (f.isDirectory()) {
+            File[] files = f.listFiles();
+            if (files != null) {
+                Arrays.stream(files).forEach(file -> list(file, depth + 1));
+            }
+        }
+    }
+
+    private String inset(int depth) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < depth; ++i) {
+            builder.append("    ");
+        }
+        return builder.toString();
+    }
 }
